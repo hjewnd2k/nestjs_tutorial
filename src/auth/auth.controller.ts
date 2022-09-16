2d7c94d4f469274e84227ef015d77ab9eea3d9ae
HieuPH <hieuphamcoder@gmail.com>
2022-09-16 13:12:23 +0700

    init project

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
